fb28aa990be7492b78bff2f095c0c0a423d46aec
Adam Wagh <adamwagh@gmail.com>
2022-08-04 09:28:57 -0500

    deploying

--- server.js
+++ server.js
@@ -61,11 +61,11 @@ app.post("/api/notes", (req, res) => {
     res.json(newNote)
     }
   })
-  
+
 // ********HTML ROUTES***********
 // HTML home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public.index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
